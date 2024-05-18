1ae542c98c2c292eac86480dcea18c7024264ede
armanali.9brainz <armanali.9brainz@gmail.com>
2024-05-18 09:23:57 +0530

    chatbot api bux fixing

--- server/index.ts
+++ server/index.ts
@@ -13,6 +13,54 @@ const StartServer = async () =>{
     await App(app);
 
 
+    const dialogflow = require('@google-cloud/dialogflow');
+    const uuid = require('uuid');
+    
+    /**
+     * Send a query to the dialogflow agent, and return the query result.
+     * @param {string} projectId The project to be used
+     */
+    async function runSample(projectId = 'deliverypoint-bot-k9dr') {
+      // A unique identifier for the given session
+      const sessionId = uuid.v4();
+    
+      // Create a new session
+      const sessionClient = new dialogflow.SessionsClient({
+        keyFilename :"/home/backend/All-React-Projects/FoodOrderApp/server/deliverypoint-bot-k9dr-673b608529a3.json"
+      });
+      const sessionPath = sessionClient.projectAgentSessionPath(
+        projectId,
+        sessionId
+      );
+    
+      // The text query request.
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: 'How do I place an order using the app?',
+            // The language used by the client (en-US)
+            languageCode: 'en-US',
+          },
+        },
+      };
+    
+      // Send request and log result
+      const responses = await sessionClient.detectIntent(request);
+      console.log('Detected intent');
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+        console.log('  No intent matched.');
+      }
+    }
+    runSample()
+
+
 
 
     app.listen(PORT , () => {
