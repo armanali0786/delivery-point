69230f68ff7a6b7f4466f0a8e53430ef348ba601
armanali.9brainz <armanali.9brainz@gmail.com>
2024-05-28 16:44:10 +0530

    generated build

--- server/dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STRIPE_SK = exports.PORT = exports.JWT_SECRET = exports.MONGO_URI = void 0;
-exports.MONGO_URI = "mongodb+srv://admin:admin123@cluster0.zxbyl9m.mongodb.net/FoodOrderApp?retryWrites=true&w=majority";
-exports.JWT_SECRET = "myfoodappsecret";
-exports.PORT = process.env.PORT || 8080;
-exports.STRIPE_SK = "sk_test_51PDk1pSGUBw5Yv8Zh3V8noodW9lBOLj14cwQPQfg31WYXKP9v64POGBbzdNHcqiGdrnTO4fi3KNeFU6rNd0snJIr00yiDKFESZ";
